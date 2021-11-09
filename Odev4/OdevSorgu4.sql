3d24bb0b20a8a89f29ccc8430ea44d1f9563ab9f
unallerhann <93905756+unallerhann@users.noreply.github.com>
2021-11-09 16:05:11 +0300

    Add files via upload

---
+++ Odev4/OdevSorgu4.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT replacement_cost from film;
+SELECT COUNT(DISTINCT replacement_cost) from film;
+SELECT COUNT(*) FROM film where  title LIKE 'T%' AND rating='G'
+SELECT COUNT(*) from country where country LIKE '_____'
+SELECT COUNT(*) from city where city ILIKE '%r'
