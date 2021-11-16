8ced7f4da402398f19e403b8e9ff91ffe653d05f
unallerhann <93905756+unallerhann@users.noreply.github.com>
2021-11-16 15:58:47 +0300

    Delete Sorgu6.sql

--- Sorgu6.sql
+++
@@ -1,4 +0,0 @@
-SELECT AVG(rental_rate) from film;
-SELECT COUNT(*) from film WHERE title LIKE 'C%';
-SELECT MAX(length) from film WHERE rental_rate=0.99;
-SELECT COUNT(DISTINCT replacement_cost) FROM film WHERE length>150;
